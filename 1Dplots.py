33e5e2d7e4999c476b13699e7b3698ccb4bc1690
MurtazAlek <aleksandrapilipenko@gmail.com>
2021-12-07 23:21:56 +0300

    1d plot

--- 1Dplots.py
+++ 1Dplots.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from keras.models import load_model
-
+import matplotlib.pyplot as plt
+import random
 from keras import metrics
 from keras import losses
 import tensorflow as tf
@@ -15,7 +16,8 @@ from numpy import linalg as LA
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-batch_size = 2000
+batch_size_small = 128
+batch_size_big = 2000
 epochs = 300
 num_classes = 10
 
@@ -50,13 +52,15 @@ x_train=x_train[indexes_train]
 y_train=y_train[indexes_train]
 
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+train_dataset_small = train_dataset.shuffle(buffer_size=1024).batch(batch_size_small)
+train_dataset_big = train_dataset.shuffle(buffer_size=1024).batch(batch_size_big)
 
 test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-test_dataset = test_dataset.shuffle(buffer_size=1024).batch(batch_size)
+test_dataset_small = test_dataset.shuffle(buffer_size=1024).batch(batch_size_small)
+test_dataset_big = test_dataset.shuffle(buffer_size=1024).batch(batch_size_big)
 
-val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_dataset = val_dataset.shuffle(buffer_size=1024).batch(batch_size)
+# val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+# val_dataset = val_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
 loss_fn = keras.losses.CategoricalCrossentropy(from_logits=False)
 #opt = tfa.optimizers.SGDW( weight_decay=0.0005, momentum=0.9, clipnorm=1)
@@ -64,37 +68,75 @@ opt = tf.keras.optimizers.Adam(beta_1=0.9, beta_2=0.999, epsilon=1e-07)
 metrica = keras.metrics.CategoricalAccuracy(name='Acc')
 
 
-saved_model_big=load_model(r'D:\Loss\ResNet20_SHCUT_2000_v2\saved_models\cifar10_ResNet20v1_model.300.h5')
-saved_model_small=load_model(r'D:\Loss\ResNet20_SHCUT_128_v2\saved_models\cifar10_ResNet20v1_model.300.h5')
-saved_model_big.compile(loss=loss_fn,optimizer=opt,metrics=metrica)
-saved_model_small.compile(loss=loss_fn,optimizer=opt,metrics=metrica)
+saved_model_small=load_model(r'D:\Loss\Res_Net20_NOSHCUT_128_v2\saved_models\cifar10_ResNet20_NOSHCUT_128_300.h5')
+saved_model_big = load_model(r'D:\Loss\ResNet20_NOSHCUT_2000\saved_models\cifar10_ResNet20_NOSHCUT_2000_300.h5')
 
-
-weight_big=saved_model_big.get_weights()
 weight_small=saved_model_small.get_weights()
-xmin, xmax, xnum = -1, 2, 10
-alpha=np.linspace(xmin, xmax, xnum)
-def d1plot(weight_big,weight_small,alpha, dataset, y):
-    losses = []
-    accuracy = []
-    for a in alpha:
-        print(a)
-        new_weights=[]
-        for w_b,w_s in zip(weight_big,weight_small):
-            w_n=w_s+a*(w_b-w_s)
-            print(min(w_n.ravel()), max(w_n.ravel()))
-            new_weights.append(w_n)
-        saved_model_small.set_weights(new_weights)
-        y_pred = saved_model_small.predict(dataset)
-        print(y_pred)
-        cce = tf.keras.losses.CategoricalCrossentropy()
-        loss = cce(y, y_pred).numpy()
-        acc = 1 - (np.sum(np.abs(y_pred - y))) / (2 * y.shape[0])
-        saved_model_small.set_weights(weight_small)
-        losses.append(loss)
-        accuracy.append(acc)
-    return losses, accuracy
-print(d1plot(weight_big,weight_small,alpha,train_dataset,y_train))
+weight_big = saved_model_big.get_weights()
+
+
+xmin, xmax, xnum = -1, 1, 31
+alphas=np.linspace(xmin, xmax, xnum)
+
+def Loss_BS(model,alphas,weights,test_dataset,train_dataset):
+    np.random.seed(123)
+    cce = tf.keras.losses.CategoricalCrossentropy()
+    weight_init = [np.random.normal(loc=0.0, scale=1.0, size=w.shape) for w in weights]
+    Loss_train=[]
+    Loss_test=[]
+    #Acc_train=[]
+    #Acc_test=[]
+    for alpha in alphas:
+
+        new_weights = []
+        for w_b, w_init in zip(weights,weight_init):
+            if w_b.ndim==4:
+                w =(1-alpha)*w_b+alpha*w_init
+                new_weights.append(w)
+            elif w_b.ndim==2:
+                w = (1 - alpha) * w_b + alpha * w_init
+                new_weights.append(w)
+            else:
+                w = w_b
+                #w = (1 - alpha) * w_s + alpha * w_init
+                #w = np.zeros(w_s.shape)
+                new_weights.append(w)
+
+        model.set_weights(new_weights)
+        y_pred_test = model.predict(test_dataset)
+        y_pred_train = model.predict(train_dataset)
+
+        loss_test = cce(y_test, y_pred_test).numpy()
+        loss_train = cce(y_train, y_pred_train).numpy()
+        #acc_test = 1 - (np.sum(np.abs(y_pred_test - y_test))) / (2 * y_test.shape[0])
+        #acc_train = 1 - (np.sum(np.abs(y_pred_train - y_train))) / (2 * y_train.shape[0])
+        model.set_weights(weights)
+        Loss_train.append(loss_train)
+        Loss_test.append(loss_test)
+        #Acc_train.append(acc_train)
+        #Acc_test.append(acc_test)
+    return Loss_train,Loss_test
+
+fig, ax, = plt.subplots()
+
+fig.suptitle('ResNet20_NOSHCUT_SmallBS_vs_BigBS')
+# ax.plot(alphas, Loss_BS(alphas=alphas,model=saved_model_small,weights=weight_small,test_dataset=test_dataset_small,train_dataset=train_dataset_small)[0],
+#          label='train_loss',color='blue',linestyle='solid')
+ax.plot(alphas, Loss_BS(alphas=alphas,model=saved_model_small,weights=weight_small,test_dataset=test_dataset_small,train_dataset=train_dataset_small)[1],
+         label='test_loss_128',color='blue',linestyle='dashed')
+# ax.plot(alphas, Loss_BS(alphas=alphas,model=saved_model_big,weights=weight_big,test_dataset=test_dataset_big,train_dataset=train_dataset_big)[0],
+#         label='train_loss',color='red',linestyle='solid')
+ax.plot(alphas, Loss_BS(alphas=alphas,model=saved_model_big,weights=weight_big,test_dataset=test_dataset_big,train_dataset=train_dataset_big)[1],
+        label='test_loss_2000',color='red',linestyle='dashed')
+ax.set_ylabel('Loss')
+ax.legend(loc=2)
+fig.savefig(fname='D:\Loss\Big_BS_vs_Small_BS.png', dpi=300, bbox_inches='tight', format='pdf')
+plt.show()
+
+
+
+
+
 
 
 
